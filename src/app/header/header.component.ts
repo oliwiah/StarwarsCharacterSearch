4ffa2c9fa8b22fccb3f05de3db430cbdfe84edce
oliwiah <oliwia.hajszprych@gmail.com>
2018-10-03 11:08:14 +0200

    added missing imports

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Character } from 'src/app/character.model';
+import { CharacterService } from 'src/app/StarwarsCharacter.service';
 
 @Component({
   selector: 'app-header',
